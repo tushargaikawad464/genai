486b1fc05f0dda3f661f30dddbf533ddad02f965
Tushar Gaikawad <tushargaikwad464@gmail.com>
2025-05-04 13:41:26 +0530

    region specific env file

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ from dotenv import dotenv_values, find_dotenv
 
 def get_env_vars():
     try:
-        env_vars = dotenv_values(find_dotenv())
+        env_vars = dotenv_values(find_dotenv(".env.us-east-1"))
 
         return env_vars
     except FileNotFoundError:
